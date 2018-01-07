7cf3a73bce9f85cd05f423f319c6bb0e95d9c4e3
Cody <codyjamesmcmahon@gmail.com>
2018-01-07 08:44:45 -0500

    fixed edge cases"

--- art.js
+++ art.js
@@ -1,6 +1,6 @@
 function draw(){
 	paint.clearRect(0,0,screen.width,screen.height)
-	for(var i = 0;i<students;i++){
+	for(var i = students-1;i>=0;i--){
 		paint.drawImage(all_students[i].img,all_students[i].loc.curr.x,all_students[i].loc.curr.y,50,50);
 	}
 	requestAnimationFrame(draw);
--- core.js
+++ core.js
@@ -7,18 +7,25 @@ function reset_app(){
 		all_students[i].loc.des.x = Math.floor(i/10)*50;
 		all_students[i].loc.des.y = Math.floor(i%10)*50;
 	}
+	screen.height = 500;
 }
 function launch(){
 	student_text.disabled = true;
 	group_text.disabled = true;
 	launched = true;
-	if(groups === 0){
+	if(Math.floor(students/groups) <= students%groups){
+		console.log("error!!!!!");
+		error.innerHTML = "group size impossible trying larger group(click go)";
+		group_text.value = '' + (groups + Math.floor(students/groups) - students%groups + 1);
+		console.log('' + (groups + Math.floor(students/groups) - students%groups));
+		groups += (Math.floor(students/groups) - students%groups);
+		student_text.disabled = false;
+		group_text.disabled = false;
+		launched = false;
+	}
+	else if(groups === 0){
 		console.log("ZERO Group!!!");
 		error.innerHTML = "ZERO GROUP!!";
-	}
-	else if(Math.floor(students/groups) <= students%groups){
-		console.log("error!!!!!");
-		error.innerHTML = "some error!!1";
 	}
 	else if(students < groups*2){
 		console.log("group too big!!!");
@@ -40,7 +47,7 @@ function shuffle(){
 		var desired_group = Math.floor(Math.random()*num);
 		while(desired_group < extras ? g[desired_group].length > groups  : g[desired_group].length > groups-1){
 			desired_group = Math.floor(Math.random()*num);
-			console.log(g);
+			//console.log(g);
 		}
 		g[desired_group][g[desired_group].length] = all_students[i];
 	}
@@ -50,11 +57,16 @@ function shuffle(){
 			g[i][o].loc.fut.y = 50 + Math.floor(i/3)*150 + Math.floor(o/4)*50;
 		}
 	}
+	if (num > 9){
+		num -= 9;
+		num = num/3 + 1;
+		screen.height += num*150;
+	}
 }
 function domino(c,d){
-	all_students[c].loc.des.x = all_students[c].loc.fut.x;
-	all_students[c].loc.des.y = all_students[c].loc.fut.y;
-	if(c+1<d && launched === true){
+	if(c<d && launched === true){
+		all_students[c].loc.des.x = all_students[c].loc.fut.x;
+		all_students[c].loc.des.y = all_students[c].loc.fut.y;
 		setTimeout(function(){
 			domino(c+1,d);
 		},100);
